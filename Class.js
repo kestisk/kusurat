5c2e49ae1f81c776fea42844607ef2ed8a5464be
kestisk <sezerkesti@gmail.com>
2018-06-18 10:40:21 +0300

    sezer

--- Class.js
+++ Class.js
@@ -9,7 +9,7 @@ export default class Classes extends Component {
         return (
             <Container>
                 <Content>
-                    <Text>buraygit gelecek</Text>
+                    <Text> gelecek</Text>
                     <Button rounded onPress={this.at}><Text>GERİ</Text></Button>
                 </Content>
             </Container>
