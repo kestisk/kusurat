1298b8d4a80fc37abec1ee86085bfe078166739e
kestisk <sezerkesti@gmail.com>
2018-06-18 11:13:43 +0300

    hıop

--- App.js
+++ App.js
@@ -23,7 +23,7 @@ export default class App extends Component<Props> {
           <Card style={styles.container}>
             <CardItem style={{ backgroundColor: 'rgb(244, 244, 244)' }}>
 
-              <Text>sezer</Text>
+              <Text>Sezer Abiiiii</Text>
 
             </CardItem>
             <CardItem style={{ backgroundColor: 'rgb(244, 244, 244)' }} >
