28e2ab0f3bfd42b36628ad8abc7c1da5e84fc371
kestisk <sezerkesti@gmail.com>
2018-06-21 23:11:03 +0300

    21

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 
 
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, Alert } from 'react-native';
-import { Button, Icon, Container, Content, Card, CardItem, Input, Item, Header } from 'native-base';
+import { Platform, StyleSheet, Text, View, Alert, Image } from 'react-native';
+import { Button, Icon, Container, Content, Card, CardItem, Input, Item, Header, Body, Title, Left } from 'native-base';
 const instructions = Platform.select({
   ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
   android:
@@ -15,37 +15,34 @@ type Props = {};
 export default class App extends Component<Props> {
   constructor(props) {
     super(props);
-    this.state = { text1: "isim" };
+    this.state = { text1: "KULLANICI ADI" };
   }
   render() {
     return (
-      <Container>
+      <Container style={{ backgroundColor: 'white' }}>
         <Header style={{ backgroundColor: '#62B1F6' }} >
-          <Text style={styles.heade}>KUSUR-AT</Text>
+          <Left></Left>
+          <Body><Title style={styles.heade}>KÜSUR-AT</Title></Body>
         </Header>
-        <Content contentContainerStyle={styles.contain}>
-          <Card style={styles.container}>
-            <CardItem style={{ backgroundColor: 'rgb(244, 244, 244)' }}>
-
-              <Text style={{ fontSize: 20 }}>{this.state.text1}</Text>
-
-            </CardItem>
-            <CardItem style={{ backgroundColor: 'rgb(244, 244, 244)' }} >
-
-              <Icon style={styles.containe} name='person' />
-            </CardItem>
+        <Content>
+          <Text style={{ fontSize: 20, alignSelf: 'center', marginTop: 10, marginBottom: 10 }}>{this.state.text1}</Text>
+          <Card style={{ height: 240, width: 240, backgroundColor: 'grey', alignSelf: 'center', alignItems: 'center' }}>
+            <Image style={{ height: 200, width: 200, alignSelf: 'center', marginTop:10 }} source={require('./image/kedicik.jpeg')} />
           </Card>
 
-        </Content>
-        <Content style={{ padding: 20 }}>
+
           <Item rounded style={styles.welcome}>
             <Icon active name='person' />
             <Input maxLength={12} onChangeText={(text1) => this.setState({ text1 })} placeholder='Kullanıcı Adı' />
           </Item>
-          <Button style={{ marginTop: 10 }} info block rounded onPress={this.sk}>
-            <Text style={{ color: 'white', fontSize: 20 }}>CONFİRM</Text>
-            <Icon name='wine' style={{ color: 'red' }} />
+
+
+          <Button style={{ marginTop: 10, alignSelf: 'center' }} info rounded onPress={this.sk}>
+            <Icon name='arrow-forward' style={{ color: 'white' }} />
           </Button>
+
+
+
         </Content>
 
       </Container >
@@ -58,27 +55,19 @@ export default class App extends Component<Props> {
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent: 'center',
     height: 220,
     width: 220,
     alignItems: 'center',
-    backgroundColor: 'rgb(244, 244, 244)',
-    borderWidth: 5,
+    backgroundColor: 'white',
     borderColor: 'black'
   },
   contain: {
-    justifyContent: 'center',
     alignItems: 'center'
 
 
   },
   containe: {
-    width: null,
-    fontSize: 150,
-    color: 'white',
-    textAlign: 'center',
-
-    backgroundColor: '#62B1F6'
+    backgroundColor: 'rgb(244, 244, 244)',
 
 
   },
--- Class.js
+++ Class.js
@@ -1,22 +1,56 @@
 
 import React, { Component } from 'react';
-import { Text } from 'react-native';
-import { Button, Container, Content } from 'native-base';
+import { Text, StyleSheet, Image } from 'react-native';
+import {
+    Button, Container, Content, Header, Icon, Left, Toast, Body, Title, CardItem, Card, Item
+} from 'native-base';
 
 export default class Classes extends Component {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            showToast: false
+        };
+    }
     render() {
         return (
-            <Container>
-                <Content>
-                    <Text>onur geldi</Text>
-                    <Text>attt</Text>
-                    <Button rounded onPress={this.at}><Text>GERİ</Text></Button>
+            <Container style={{ backgroundColor: 'white' }}>
+                <Header style={{ backgroundColor: '#62B1F6' }}><Left><Button transparent onPress={this.at}><Icon name='arrow-back' /></Button></Left>
+                    <Body><Title style={styles.heade}>KÜSUR-AT</Title></Body>
+
+                </Header>
+                <Content style={{ padding: 20 }}>
+                    <Button style={{ marginTop: 10 }} info block rounded ><Text style={styles.heade}>4.SINIF</Text></Button>
+                    <Button style={{ marginTop: 10 }} info block rounded onPress={this.at}><Text style={styles.heade}>5.SINIF</Text></Button>
+                    <Button style={{ marginTop: 10 }} info block rounded onPress={this.at}><Text style={styles.heade}>6.SINIF</Text></Button>
+                    <Button style={{ marginTop: 10 }} info block rounded onPress={this.at}><Text style={styles.heade}>7.SINIF</Text></Button>
+                    <Button style={{ marginTop: 10 }} info block rounded onPress={this.at}><Text style={styles.heade}>8.SINIF</Text></Button>
+
+
+
+                    <Image style={{ height: 150, width: 150, alignSelf: 'center', marginTop: 20 }} source={require('./image/kedicik.jpeg')} />
+
+
                 </Content>
             </Container>
         );
     }
     at = () => {
         this.props.navigation.navigate('Main');
+    };
+    toas = () => {
+        Toast.show({
+            text: "Wrong password!",
+            buttonText: "Okay",
+            position: "bottom"
+        });
     }
 }
+const styles = StyleSheet.create({
+    heade: {
+        textAlign: "center",
+        color: 'white',
+        fontSize: 30,
+        padding: 5
+    }
+});
